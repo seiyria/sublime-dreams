2fe6b471a531076d120d833f5a1e71c0f9e9c94e
Kyle <kyle@tekdice.com>
2013-08-11 17:57:53 -0500

    adding dream seeker and dream daemon run

--- dmc.py
+++ dmc.py
@@ -9,10 +9,17 @@ path = {
 
 class DmcCommand(sublime_plugin.WindowCommand):
 
-	def run(self, cmd = [], file_regex = ""):
+	def run(self, cmd = [], file_regex = "", dream_seeker = False, dream_daemon = False):
 		dmpath = path[sublime.arch()]
 
-		self.build(dmpath, cmd[0], file_regex)
+		dme_dir = self.build(dmpath, cmd[0], file_regex)
+		dmb_dir = self.find_dmb(dme_dir)
+
+		if dream_seeker:
+			self.run_in_seeker(dmpath, dmb_dir)
+
+		if dream_daemon:
+			self.run_in_daemon(dmpath, dmb_dir)
 
 	def build(self, environment_path, file, file_regex):
 
@@ -25,6 +32,20 @@ class DmcCommand(sublime_plugin.WindowCommand):
 			'working_dir': dirname(dme_path)
 		}
 		sublime.active_window().run_command("exec", args)
+
+		return dirname(dme_path)
+
+	def find_dmb(self, current_dir):
+
+		#TODO match the current directory name/dme name
+		dmb_list = [ 
+			current_dir+"\\"+f.encode('ascii', 'ignore') 
+				for f in listdir(current_dir) 
+					if isfile(join(current_dir, f)) and splitext(f)[1] == u".dmb" 
+			]
+
+		if len(dmb_list) is not 0:
+			return dmb_list[0]
 
 	def find_closest_dme(self, compile_file):
 		current_dir = compile_file
@@ -51,8 +72,16 @@ class DmcCommand(sublime_plugin.WindowCommand):
 	def drive_root(self, path):
 		return splitdrive(path)[0]+"\\"
 
-	def run_in_seeker():
-		print 'asdf'
+	def run_in_seeker(self, environment_path, dmb):
+		new_cmd = [ environment_path + "dreamseeker.exe" , dmb ]
+		args = {
+			'cmd': new_cmd
+		}
+		sublime.active_window().run_command("exec", args)
 
-	def run_in_daemon():
-		print 'sdf'
+	def run_in_daemon(self, environment_path, dmb):
+		new_cmd = [ environment_path + "dreamdaemon.exe" , dmb , "-trusted" ]
+		args = {
+			'cmd': new_cmd
+		}
+		sublime.active_window().run_command("exec", args)
