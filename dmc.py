620f659066f506d0659dc0e085a97386b6ec69a6
Kyle Kemp <kyle@seiyria.com>
2013-08-15 10:16:48 -0500

    merge all files into dmc.py

--- dmc.py
+++ dmc.py
@@ -5,13 +5,20 @@ import thread
 import subprocess
 import functools
 import time
-import asynclistener
 import processlistener
+import asynclistener
 
 path = {
 	'x32': "C:/Program Files/BYOND/bin/",
 	'x64': "C:/Program Files (x86)/BYOND/bin/"
 }
+
+class ProcessListener(object):
+    def on_data(self, proc, data):
+        pass
+
+    def on_finished(self, proc):
+        pass
 
 class DmcCommand(sublime_plugin.WindowCommand, ProcessListener):
 
@@ -236,3 +243,83 @@ class DmcCommand(sublime_plugin.WindowCommand, ProcessListener):
 
 	def on_finished(self, proc):
 		sublime.set_timeout(functools.partial(self.finish, proc), 0)
+
+# Encapsulates subprocess.Popen, forwarding stdout to a supplied
+# ProcessListener (on a separate thread)
+class AsyncProcess(object):
+    def __init__(self, arg_list, env, listener,
+            # "path" is an option in build systems
+            path="",
+            # "shell" is an options in build systems
+            shell=False):
+
+        self.listener = listener
+        self.killed = False
+
+        self.start_time = time.time()
+
+        # Hide the console window on Windows
+        startupinfo = None
+        if os.name == "nt":
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+        # Set temporary PATH to locate executable in arg_list
+        if path:
+            old_path = os.environ["PATH"]
+            # The user decides in the build system whether he wants to append $PATH
+            # or tuck it at the front: "$PATH;C:\\new\\path", "C:\\new\\path;$PATH"
+            os.environ["PATH"] = os.path.expandvars(path).encode(sys.getfilesystemencoding())
+
+        proc_env = os.environ.copy()
+        proc_env.update(env)
+        for k, v in proc_env.iteritems():
+            proc_env[k] = os.path.expandvars(v).encode(sys.getfilesystemencoding())
+
+        self.proc = subprocess.Popen(arg_list, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE, startupinfo=startupinfo, env=proc_env, shell=shell)
+
+        if path:
+            os.environ["PATH"] = old_path
+
+        if self.proc.stdout:
+            thread.start_new_thread(self.read_stdout, ())
+
+        if self.proc.stderr:
+            thread.start_new_thread(self.read_stderr, ())
+
+    def kill(self):
+        if not self.killed:
+            self.killed = True
+            self.proc.terminate()
+            self.listener = None
+
+    def poll(self):
+        return self.proc.poll() == None
+
+    def exit_code(self):
+        return self.proc.poll()
+
+    def read_stdout(self):
+        while True:
+            data = os.read(self.proc.stdout.fileno(), 2**15)
+
+            if data != "":
+                if self.listener:
+                    self.listener.on_data(self, data)
+            else:
+                self.proc.stdout.close()
+                if self.listener:
+                    self.listener.on_finished(self)
+                break
+
+    def read_stderr(self):
+        while True:
+            data = os.read(self.proc.stderr.fileno(), 2**15)
+
+            if data != "":
+                if self.listener:
+                    self.listener.on_data(self, data)
+            else:
+                self.proc.stderr.close()
+                break
