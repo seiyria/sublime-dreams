60842b28fc3836ed78936709f10a2d5e2c440a55
Kyle <kyle@tekdice.com>
2013-08-14 21:39:26 -0500

    dream seeker previous process kill = working!!!

--- dmc.py
+++ dmc.py
@@ -39,33 +39,12 @@ class DmcCommand(sublime_plugin.WindowCommand, ProcessListener):
 		dmb_dir = self.find_dmb(dme_dir)
 
 		if dream_seeker:
+			if self.dream_seeker and kill_old:
+				self.dream_seeker.kill()
 			self.run_in_seeker(dmpath, dmb_dir)
 
 		if dream_daemon:
 			self.run_in_daemon(dmpath, dmb_dir)
-
-	def setup_sublime(self, file_regex, line_regex, working_dir, encoding):
-		if not hasattr(self, 'output_view'):
-		    self.output_view = self.window.get_output_panel("exec")
-
-		if (working_dir == "" and self.window.active_view()
-		                and self.window.active_view().file_name()):
-		    working_dir = os.path.dirname(self.window.active_view().file_name())
-
-		self.output_view.settings().set("result_file_regex", file_regex)
-		self.output_view.settings().set("result_line_regex", line_regex)
-		self.output_view.settings().set("result_base_dir", working_dir)
-
-		self.window.get_output_panel("exec")
-
-		self.encoding = encoding
-
-		show_panel_on_build = sublime.load_settings("Preferences.sublime-settings").get("show_panel_on_build", True)
-		if show_panel_on_build:
-		    self.window.run_command("show_panel", {"panel": "output.exec"})
-
-		if working_dir != "":
-		    os.chdir(working_dir)
 
 	def run_cmd(self, cmd, is_daemon = False, is_seeker = False, is_maker = False, **kwargs):
 
@@ -82,9 +61,18 @@ class DmcCommand(sublime_plugin.WindowCommand, ProcessListener):
 		    err_type = WindowsError
 
 		try:
+			# I have a large dislike of writing it this way
 			if is_maker:
 				sublime.status_message("Building DMB...")
 				self.proc = AsyncProcess(cmd, merged_env, self, **kwargs)
+
+			if is_seeker:					
+				sublime.status_message("Running in DreamSeeker...")
+				self.dream_seeker = AsyncProcess(cmd, merged_env, self, **kwargs)
+
+			if is_daemon:					
+				sublime.status_message("Running in DreamDaemon...")
+				self.dream_daemon = AsyncProcess(cmd, merged_env, self, **kwargs)
 
 		except err_type as e:
 		    self.append_data(None, str(e) + "\n")
@@ -97,32 +85,23 @@ class DmcCommand(sublime_plugin.WindowCommand, ProcessListener):
 		    if not self.quiet:
 		        self.append_data(None, "[Finished]")
 
-		    sublime.status_message("Failed doing a thing")
+#build runners
 
 	def build(self, environment_path, dme_path):
 
-		cmd = [ environment_path + "dm.exe" , dme_path ]
-		'''
-		args = {
-			'cmd': new_cmd,
-			'file_regex': file_regex,
-			'working_dir': dirname(dme_path)
-		}
-		'''
+		cmd = [ environment_path + "dm" , dme_path ]
+
 		self.run_cmd(cmd, is_maker = True)
 
-	def find_dmb(self, current_dir):
-		#sublime.status_message("Finding closest DMB")
+	def run_in_seeker(self, environment_path, dmb = ''):
+		cmd = [ environment_path + "dreamseeker" , dmb ]
+		self.run_cmd(cmd, is_seeker = True)
 
-		#TODO match the current directory name/dme name
-		dmb_list = [ 
-			current_dir+"\\"+f.encode('ascii', 'ignore') 
-				for f in os.listdir(current_dir) 
-					if isfile(join(current_dir, f)) and splitext(f)[1] == u".dmb" 
-			]
+	def run_in_daemon(self, environment_path, dmb):
+		cmd = [ environment_path + "dreamdaemon" , dmb , "-trusted" ]
+		self.run_cmd(cmd, is_daemon = True)
 
-		if len(dmb_list) is not 0:
-			return dmb_list[0]
+#file finders
 
 	def find_closest_dme(self, compile_file):
 		sublime.status_message("Finding closest DME...")
@@ -147,27 +126,49 @@ class DmcCommand(sublime_plugin.WindowCommand, ProcessListener):
 
 		return dme.encode('ascii', 'ignore') 
 
+
+	def find_dmb(self, current_dir):
+		#TODO match the current directory name/dme name
+		dmb_list = [ 
+			current_dir+"\\"+f.encode('ascii', 'ignore') 
+				for f in os.listdir(current_dir) 
+					if isfile(join(current_dir, f)) and splitext(f)[1] == u".dmb" 
+			]
+
+		if len(dmb_list) is not 0:
+			return dmb_list[0]
+
+#get the drive root
+
 	def drive_root(self, path):
 		return splitdrive(path)[0]+"\\"
 
-	def run_in_seeker(self, environment_path, dmb = ''):
-		sublime.status_message("Running "+dmb+"in DreamSeeker...")
-		new_cmd = [ environment_path + "dreamseeker.exe" , dmb ]
-		args = {
-			'cmd': new_cmd
-		}
-		sublime.active_window().run_command("exec", args)
+#sublime configuration
 
-	def run_in_daemon(self, environment_path, dmb):
-		sublime.status_message("Running in Dream Daemon")
-		new_cmd = [ environment_path + "dreamdaemon.exe" , dmb , "-trusted" ]
-		args = {
-			'cmd': new_cmd
-		}
-		sublime.active_window().run_command("exec", args)
+	def setup_sublime(self, file_regex, line_regex, working_dir, encoding):
+		if not hasattr(self, 'output_view'):
+		    self.output_view = self.window.get_output_panel("exec")
 
+		if (working_dir == "" and self.window.active_view()
+		                and self.window.active_view().file_name()):
+		    working_dir = os.path.dirname(self.window.active_view().file_name())
 
-# from exec.py
+		self.output_view.settings().set("result_file_regex", file_regex)
+		self.output_view.settings().set("result_line_regex", line_regex)
+		self.output_view.settings().set("result_base_dir", working_dir)
+
+		self.window.get_output_panel("exec")
+
+		self.encoding = encoding
+
+		show_panel_on_build = sublime.load_settings("Preferences.sublime-settings").get("show_panel_on_build", True)
+		if show_panel_on_build:
+		    self.window.run_command("show_panel", {"panel": "output.exec"})
+
+		if working_dir != "":
+		    os.chdir(working_dir)
+
+# from exec.py verbatim
 
 	def is_enabled(self, kill = False):
 		if kill:
